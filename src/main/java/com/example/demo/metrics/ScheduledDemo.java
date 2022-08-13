64c67272e902a8e9fba230507cd721c7659a5783
Administrator <admin@example.com>
2022-08-13 16:21:08 +0800

    change class a java class

--- src/main/java/com/example/demo/metrics/ScheduledDemo.java
+++ src/main/java/com/example/demo/metrics/ScheduledDemo.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class girScheduledDemo {
+public class ScheduledDemo {
 
 
     private Integer count1 = 0;
